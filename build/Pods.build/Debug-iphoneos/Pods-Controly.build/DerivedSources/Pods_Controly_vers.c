6337d64cbfe68772bf5b78f065a267b1a03abf02
refaelach <refael.lach@s-access.com>
2017-08-06 16:24:46 -0700

    First Commit
    Creation of the project)

---
+++ build/Pods.build/Debug-iphoneos/Pods-Controly.build/DerivedSources/Pods_Controly_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_ControlyVersionString[];
+ extern const double Pods_ControlyVersionNumber;
+
+ const unsigned char Pods_ControlyVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_Controly  PROJECT:Pods-1" "\n";
+ const double Pods_ControlyVersionNumber __attribute__ ((used)) = (double)1.;
