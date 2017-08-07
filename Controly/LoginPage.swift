3979580c4fdb873cd52208183855e0b76a4de8cb
refaelach <refael.lach@s-access.com>
2017-08-06 20:10:37 -0700

    Finished UI (No constraints) - Login Page
    Finished UI (No constraints) - Login Page

--- Controly/LoginPage.swift
+++ Controly/LoginPage.swift
@@ -8,28 +8,82 @@
 
 import UIKit
 
-class LoginPage: UIViewController {
+class LoginPage: UIViewController, UITextFieldDelegate {
 
+    
+    @IBOutlet weak var EmailText: UITextField!
+    
+    @IBOutlet weak var LogoUpConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var TextfieldBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var UpperLogo: UIImageView!
+    
+    @IBOutlet weak var LblTopConstraint: NSLayoutConstraint!
+    
+    
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        EmailText.delegate = self
 
-        // Do any additional setup after loading the view.
+        let mycolor = UIColor(red:0.32, green:0.69, blue:0.90, alpha:1.0)
+        
+        
+        EmailText.layer.cornerRadius = 20
+        EmailText.layer.masksToBounds = true
+        EmailText.layer.borderColor = mycolor.cgColor
+        EmailText.layer.borderWidth = 2.0
+
+            
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func EditBegin(_ sender: Any) {
+        
+        
+        UIView.animate(withDuration: 1.5, animations: {
+            self.LogoUpConstraint.constant = 0
+            self.TextfieldBottomConstraint.constant = 230
+            self.LblTopConstraint.constant = -10
+        
+        })
+        
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        UIView.animate(withDuration: 2.5, animations: {
+            self.LogoUpConstraint.constant = 25
+            self.TextfieldBottomConstraint.constant = 40
+            self.LblTopConstraint.constant = 20
+        })
+        
+        self.view.endEditing(true)
     }
-    */
+   
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        EmailText.resignFirstResponder()
+        
+         UIView.animate(withDuration: 2.5, animations: {
+        self.LogoUpConstraint.constant = 25
+        self.TextfieldBottomConstraint.constant = 40
+        self.LblTopConstraint.constant = 20
+        })
+        
+        
+        return true
+    }
+    
+    
+    
+    
+
+    
 
 }
